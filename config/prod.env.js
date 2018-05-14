8493edd7ba61a8419984dde8f46eea2f492a2266
unknown <songcheng@yintong.com.cn>
2018-05-14 13:28:06 +0800

    a

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     LOCAL_HOST: '"http://140.82.3.174:6200/"',
-    API_HOST: '"https://140.82.3.174:6210/api"',
+    API_HOST: '"http://140.82.3.174:6210/api"',
     APP_NAME: '"房源库"'
 }
